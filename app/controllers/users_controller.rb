2fbc65e7aa431ae3fa82abffee5c32b50179bc13
s-abdelmaguid <30833686+s-abdelmaguid@users.noreply.github.com>
2018-01-08 18:34:07 +0200

    check current user before edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+   before_action :correct_user,   only: [:edit, :update]
   before_action :set_user, only: [:show, :edit, :update, :destroy]
   # GET /users
   # GET /users.json
@@ -86,6 +88,18 @@ class UsersController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_user
       # @user = User.find(params[:id])
+    end
+  
+   def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+  
+  def correct_user
+      @user = User.find(params[:id])
+       redirect_to(root_url) unless current_user?(@user)
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.

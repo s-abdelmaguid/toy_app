5aefb5a34e1673a54a203cc56cb81e54f12014b9
sabreen.99 <sabreen.ahmed.222@gmail.com>
2018-01-08 09:14:29 +0200

    finish user sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,11 +9,13 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
+    @user = User.find(params[:id])
+
   end
 
   # GET /users/new
   def new
-  #  @user = User.new
+   @user = User.new
   end
 
   # GET /users/1/edit
@@ -23,6 +25,16 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
+
+    @user = User.new(user_params)    # Not the final implementation!
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+       redirect_to @user
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+
     # @user = User.new(user_params)
     #
     # respond_to do |format|
@@ -68,6 +80,7 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
+     params.require(:user).permit(:name, :email, :password, :password_confirmation)
       # params.require(:user).permit(:name, :email)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_01_02_102506) do
+ActiveRecord::Schema.define(version: 2018_01_04_103011) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -45,6 +45,8 @@ ActiveRecord::Schema.define(version: 2018_01_02_102506) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end

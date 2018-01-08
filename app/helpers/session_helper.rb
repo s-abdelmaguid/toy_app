5a825bc644e171680cbf90dbe89a61e8382d2aa8
s-abdelmaguid <30833686+s-abdelmaguid@users.noreply.github.com>
2018-01-08 15:59:42 +0200

    define logout

--- app/helpers/session_helper.rb
+++ app/helpers/session_helper.rb
@@ -3,6 +3,11 @@ module SessionHelper
   def log_in(user)
     session[:user_id] = user.id
   end
+  
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
 
   def current_user
     if @current_user.nil?

d47cc79dcf0aa121afe984825af611b68124cca6
sabreen.99 <sabreen.ahmed.222@gmail.com>
2018-01-09 12:30:03 +0200

    until account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,8 @@ class UsersController < ApplicationController
   before_action :admin_user,     only: :destroy
   # GET /users
   # GET /users.json
-  
-  
+
+
   def index
     @users = User.paginate(page: params[:page])
   end
@@ -15,7 +15,6 @@ class UsersController < ApplicationController
   # GET /users/1.json
   def show
     @user = User.find(params[:id])
-
   end
 
   # GET /users/new
@@ -28,34 +27,18 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  # POST /users
-  # POST /users.json
   def create
     @user = User.new(user_params)    # Not the final implementation!
     if @user.save
-      log_in @user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_back_or user
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
       # Handle a successful save.
     else
       render 'new'
     end
-
-    # @user = User.new(user_params)
-    #
-    # respond_to do |format|
-    #   if @user.save
-    #     format.html { redirect_to @user, notice: 'User was successfully created.' }
-    #     format.json { render :show, status: :created, location: @user }
-    #   else
-    #     format.html { render :new }
-    #     format.json { render json: @user.errors, status: :unprocessable_entity }
-    #   end
-    # end
   end
 
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
   def update
      @user = User.find(params[:id])
     if @user.update_attributes(user_params)
@@ -65,28 +48,12 @@ class UsersController < ApplicationController
     else
       render 'edit'
     end
-    # respond_to do |format|
-    #   if @user.update(user_params)
-    #     format.html { redirect_to @user, notice: 'User was successfully updated.' }
-    #     format.json { render :show, status: :ok, location: @user }
-    #   else
-    #     format.html { render :edit }
-    #     format.json { render json: @user.errors, status: :unprocessable_entity }
-    #   end
-    # end
   end
 
-  # DELETE /users/1
-  # DELETE /users/1.json
   def destroy
     User.find(params[:id]).destroy
     flash[:success] = "User deleted"
     redirect_to users_url
-    # @user.destroy
-    # respond_to do |format|
-    #   format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-    #   format.json { head :no_content }
-    # end
   end
 
   private
@@ -94,7 +61,7 @@ class UsersController < ApplicationController
     def set_user
       # @user = User.find(params[:id])
     end
-  
+
    def logged_in_user
       unless logged_in?
         store_location
@@ -102,7 +69,7 @@ class UsersController < ApplicationController
         redirect_to login_url
       end
     end
-  
+
   def correct_user
       @user = User.find(params[:id])
        redirect_to(root_url) unless current_user?(@user)
@@ -113,7 +80,7 @@ class UsersController < ApplicationController
      params.require(:user).permit(:name, :email, :password, :password_confirmation)
       # params.require(:user).permit(:name, :email)
     end
-  
+
   def admin_user
     redirect_to(root_url) unless current_user.admin?
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_01_04_103011) do
+ActiveRecord::Schema.define(version: 2018_01_09_091501) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -46,6 +46,11 @@ ActiveRecord::Schema.define(version: 2018_01_04_103011) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin"
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 

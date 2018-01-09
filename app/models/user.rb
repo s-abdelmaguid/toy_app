672916c97d7aed4709843a580111f5f2737754d2
sabreen.99 <sabreen.ahmed.222@gmail.com>
2018-01-09 16:04:20 +0200

    seed microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,16 @@
 class User < ApplicationRecord
-  has_many :microposts
-  attr_accessor :remember_token, :activation_token, :reset_token
-  before_save { self.email = email.downcase }
+  has_many :microposts, dependent: :destroy
 
+  attr_accessor :remember_token, :activation_token, :reset_token
+
+  before_save { self.email = email.downcase }
   before_create :create_activation_digest
 
   validates :name,  presence: true, length: { maximum: 50 }
-
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-
   has_secure_password
  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
